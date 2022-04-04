fe181089c285c79c4b46f6fe7c33dc6dcf309c4c
mahboobe68 <mahbooberezaee68@gmail.com>
2022-04-04 09:08:21 +0430

    Add files via upload

---
+++ DatabaseSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//creational design pattern
+namespace Design_patterns
+{
+    public sealed class DatabaseSingleton
+    {
+
+        private static readonly Lazy<DatabaseSingleton> _instance = new Lazy<DatabaseSingleton>(() => new DatabaseSingleton());
+
+        private DatabaseSingleton() { }
+        public static DatabaseSingleton Instance 
+        {
+           get {
+                return _instance.Value;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Design_patterns;
+
+Console.WriteLine("Hello, World!");
+var DB = DatabaseSingleton.Instance;
+
+ 
+
